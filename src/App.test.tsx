56f96c63bd7cf3f3bd6f783857962ad6c0ea5e78
Belbin G K. <1014@trenser.com>
2025-04-02 16:48:43 +0530

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Belbin GK/i);
+  expect(linkElement).toBeInTheDocument();
+});
